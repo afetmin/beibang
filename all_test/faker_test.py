c1ce7fa963e977dd9682b87db40c7739b1048339
afetmin <kingpenguins@163.com>
2018-02-24 09:24:32 +0800

    Signed-off-by: afetmin <kingpenguins@163.com>

---
+++ all_test/faker_test.py
@@ -0,0 +1,12 @@
+# s = """<url>
+# <loc>http://www.roadconstuctionmachine.com/machine/{}.html</loc>
+# <priority>0.6</priority>
+# <lastmod>2018-02-11T10:14:10+00:00</lastmod>
+# <changefreq>Always</changefreq>
+# </url>
+# """
+
+# with open('sitemap.xml','w') as f:
+#     for i in range(1,5069):
+#         f.write(s.format(i))
+
